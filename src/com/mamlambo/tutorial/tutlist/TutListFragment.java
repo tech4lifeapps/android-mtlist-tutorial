cf08ac3d6edc741005c6a6be11198a212e27b8c0
kf6nvr <kf6nvr@gmail.com>
2011-05-01 03:03:07 +0000

    Changes for Version code 8:
    - add preferences screen
    - allow user to turn on and off background updating via this screen

--- src/com/mamlambo/tutorial/tutlist/TutListFragment.java
+++ src/com/mamlambo/tutorial/tutlist/TutListFragment.java
@@ -30,13 +30,7 @@
  */
 package com.mamlambo.tutorial.tutlist;
 
-import java.util.Calendar;
-import java.util.TimeZone;
-
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -55,7 +49,6 @@ import android.widget.ListView;
 
 import com.mamlambo.tutorial.tutlist.data.TutListDatabase;
 import com.mamlambo.tutorial.tutlist.data.TutListProvider;
-import com.mamlambo.tutorial.tutlist.receiver.AlarmReceiver;
 import com.mamlambo.tutorial.tutlist.service.TutListDownloaderService;
 
 public class TutListFragment extends ListFragment implements
@@ -113,49 +106,40 @@ public class TutListFragment extends ListFragment implements
 
     // options menu
 
-    private int refreshMenuId;
-
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        Intent intent = new Intent(getActivity().getApplicationContext(),
-                TutListDownloaderService.class);
-        intent.setData(Uri
-                .parse("http://feeds.feedburner.com/MobileTuts?format=xml"));
         inflater.inflate(R.menu.options_menu, menu);
+
+        // refresh menu item
+        Intent refreshIntent = new Intent(
+                getActivity().getApplicationContext(),
+                TutListDownloaderService.class);
+        refreshIntent
+                .setData(Uri
+                        .parse("http://feeds.feedburner.com/mobile-tuts-summary?format=xml"));
+
         MenuItem refresh = menu.findItem(R.id.refresh_option_item);
-        refresh.setIntent(intent);
-        refreshMenuId = refresh.getItemId();
+        refresh.setIntent(refreshIntent);
+
+        // pref menu item
+        Intent prefsIntent = new Intent(getActivity().getApplicationContext(),
+                TutListPreferencesActivity.class);
+
+        MenuItem preferences = menu.findItem(R.id.settings_option_item);
+        preferences.setIntent(prefsIntent);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == refreshMenuId) {
+        switch (item.getItemId()) {
+        case R.id.refresh_option_item:
             getActivity().startService(item.getIntent());
-
-            // also set alarm
-            Context context = getActivity().getApplicationContext();
-            setRecurringAlarm(context);
+            break;
+        case R.id.settings_option_item:
+            getActivity().startActivity(item.getIntent());
+            break;
         }
         return true;
-    }
-
-    private void setRecurringAlarm(Context context) {
-
-        // we know mobiletuts updates at right around 1130 GMT.
-        // let's grab new stuff at around 11:45 GMT, inexactly
-        Calendar updateTime = Calendar.getInstance();
-        updateTime.setTimeZone(TimeZone.getTimeZone("GMT"));
-        updateTime.set(Calendar.HOUR_OF_DAY, 11);
-        updateTime.set(Calendar.MINUTE, 45);
-
-        Intent downloader = new Intent(context, AlarmReceiver.class);
-        PendingIntent recurringDownload = PendingIntent.getBroadcast(context,
-                0, downloader, PendingIntent.FLAG_CANCEL_CURRENT);
-        AlarmManager alarms = (AlarmManager) getActivity().getSystemService(
-                Context.ALARM_SERVICE);
-        alarms.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                updateTime.getTimeInMillis(), AlarmManager.INTERVAL_DAY,
-                recurringDownload);
     }
 
     // LoaderManager.LoaderCallbacks<Cursor> methods
