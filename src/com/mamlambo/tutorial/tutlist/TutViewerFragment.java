b7ec5586b5c54a7009748f042fbb9db4a172e815
kf6nvr <kf6nvr@gmail.com>
2011-06-05 21:54:02 +0000

    Updated webview:
    * show more of the page
    * pause/resume

--- src/com/mamlambo/tutorial/tutlist/TutViewerFragment.java
+++ src/com/mamlambo/tutorial/tutlist/TutViewerFragment.java
@@ -35,17 +35,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.ZoomDensity;
 import android.webkit.WebView;
 
 public class TutViewerFragment extends Fragment {
     private WebView viewer = null;
+    
+    // if we weren't just using the compat library, we could use WebViewFragment
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         viewer = (WebView) inflater
                 .inflate(R.layout.tut_view, container, false);
+        WebSettings settings = viewer.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDefaultZoom(ZoomDensity.FAR);
+        
         return viewer;
+    }
+    
+    @Override
+    public void onPause() {
+       if (viewer != null) {
+           viewer.onPause();
+       }
+       super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (viewer != null) {
+            viewer.onResume();
+        }
     }
 
     public void updateUrl(String newUrl) {
